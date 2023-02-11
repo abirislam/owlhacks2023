508a6db6145f7eac273efad05db21df4d6445dd8
Abir Islam <abir@c8-89-f3-b8-0f-53.wireless.temple.edu>
2023-02-11 12:36:45 -0500

    settings tab options added

--- hwApp.py
+++ hwApp.py
@@ -27,27 +27,57 @@ class TabView(customtkinter.CTkTabview):
         self.label.grid(row=0, column=0, padx=20, pady=10)
 
         # 'Settings' Widgets
-        s3 = customtkinter.StringVar(value="Settings page")
-        self.label = customtkinter.CTkLabel(master=self.tab("Settings"), textvariable=s3, width=650, height=370)
-        self.label.grid(row=0, column=0, padx=20, pady=10)
+        categoryLabel = customtkinter.StringVar(value="Local Settings")
+        self.label = customtkinter.CTkLabel(master=self.tab("Settings"), textvariable=categoryLabel)
+        self.label.grid(row=0, column=0, padx=20)
+
+        # 'Settings' - Language Selector
+        language = customtkinter.StringVar(value="Language Selection")
+        self.label = customtkinter.CTkLabel(master=self.tab("Settings"), textvariable=language)
+        self.label.grid(row=1, column=0, padx=20)
+
+        langugage_selector = customtkinter.StringVar(value="English")
+        def language_callback(choice):
+            return choice
+        language_box = customtkinter.CTkComboBox(master=self.tab("Settings"),
+        values=["English", "Spanish", "German", "French"],
+            command=language_callback,
+            variable=langugage_selector)
+        language_box.grid(row=1, column=1)
+
+
+        maxDistance = customtkinter.StringVar(value="Max Distance")
+        self.label = customtkinter.CTkLabel(master=self.tab("Settings"), textvariable=maxDistance)
+        self.label.grid(row=2, column=0, padx=20, pady=10)
 
         # Distance Drop-down
-        combobox_var = customtkinter.StringVar(value="Max Distance")
+        combobox_var = customtkinter.StringVar(value="25 miles")
         def combobox_callback(choice):
             return choice
 
-        # combobox = customtkinter.CTkComboBox(master=self.tab("Settings"),
-        # value=)
+        combobox = customtkinter.CTkComboBox(master=self.tab("Settings"),
+            values=["25 miles", "50 miles", "75 miles", "100 miles"],
+            command=combobox_callback,
+            variable=combobox_var)
+        combobox.grid(row=2, column=1, pady=10)
+
+
+        # 'Settings' Doctor Options
+        doctorLabel = customtkinter.StringVar(value="Doctor Preferences")
+        self.label = customtkinter.CTkLabel(master=self.tab("Settings"), textvariable=doctorLabel)
+        self.label.grid(row=0, column=3, padx=20)
+
+        # Doctor Language Preference
 
 class App(customtkinter.CTk):
     # Setup
     def __init__(self):
         super().__init__()
-        self.geometry("720x480")
+        self.geometry("1300x740")
         self.title("Clinical Trial Connector")
 
         # Tab View Setup
-        self.tab_view = TabView(master=self)
+        self.tab_view = TabView(master=self, width=1280, height=720)
         self.tab_view.grid(row=0, column=0, padx=10, pady=10)
 
     
