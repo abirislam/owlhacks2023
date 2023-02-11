36ffa3a6bc05372e8c00c1ccafb2c202fb73cbb1
Abir Islam <abir@c8-89-f3-b8-0f-53.wireless.temple.edu>
2023-02-11 11:39:38 -0500

    initialized further tabs

--- hwApp.py
+++ hwApp.py
@@ -7,17 +7,37 @@ class TabView(customtkinter.CTkTabview):
 
         # Tabs
         self.add("Connector")
-        self.add("Sources")
+        self.add("Messages")
+        self.add("Profile")
+        self.add("Settings")
 
         # 'Connector' Widgets
         s1 = customtkinter.StringVar(value="Connector homepage")
         self.label = customtkinter.CTkLabel(master=self.tab("Connector"), textvariable=s1, width=650, height=370)
         self.label.grid(row=0, column=0, padx=20, pady=10)
 
-        # 'Sources' Widgets
-        s2 = customtkinter.StringVar(value="Pick your sources here")
-        self.label = customtkinter.CTkLabel(master=self.tab("Sources"), textvariable=s2, width=650, height=370)
+        # 'Messages' Widgets
+        messageDisplay = customtkinter.StringVar(value="Messages display")
+        self.label = customtkinter.CTkLabel(master=self.tab("Messages"), textvariable=messageDisplay, width=650, height=370)
         self.label.grid(row=0, column=0, padx=20, pady=10)
+
+        # 'Profile' Widgets
+        s2 = customtkinter.StringVar(value="Profile display")
+        self.label = customtkinter.CTkLabel(master=self.tab("Profile"), textvariable=s2, width=650, height=370)
+        self.label.grid(row=0, column=0, padx=20, pady=10)
+
+        # 'Settings' Widgets
+        s3 = customtkinter.StringVar(value="Settings page")
+        self.label = customtkinter.CTkLabel(master=self.tab("Settings"), textvariable=s3, width=650, height=370)
+        self.label.grid(row=0, column=0, padx=20, pady=10)
+
+        # Distance Drop-down
+        combobox_var = customtkinter.StringVar(value="Max Distance")
+        def combobox_callback(choice):
+            return choice
+
+        # combobox = customtkinter.CTkComboBox(master=self.tab("Settings"),
+        # value=)
 
 class App(customtkinter.CTk):
     # Setup
